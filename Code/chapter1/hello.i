a4b503dc98f1022872211c82eeacecaaa09a2b36
Jimmy Xiang <xxg1413@gmail.com>
2016-01-29 13:14:00 +0800

    add the chapter1

---
+++ Code/chapter1/hello.i
@@ -0,0 +1,7 @@
+# 1 "hello.c"
+# 1 "<command-line>"
+# 1 "hello.c"
+int main()
+{
+ printf("hello,world\n");
+}
---
+++ Code/chapter1/hello.s
@@ -0,0 +1,28 @@
+	.file	"hello.c"
+	.section	.rodata
+.LC0:
+	.string	"hello,world"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$16, %esp
+	movl	$.LC0, (%esp)
+	call	puts
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Debian 4.7.2-5) 4.7.2"
+	.section	.note.GNU-stack,"",@progbits
